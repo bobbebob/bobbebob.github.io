d04df9fd5a15cfe2b2210fb6768570b26c968465
Bob Vegter <bob.vegter@gmail.com>
2024-11-20 10:31:40 +0100

    Changed Router to HashRouter to fix 404 error when refreshing on contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./global.css";
 import HomePage from "./pages/Home.tsx";
 import PageHeader from "./components/PageHeader.tsx";
